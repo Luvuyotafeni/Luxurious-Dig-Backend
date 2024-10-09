d51b6fd5ddc310024f6ed312dea980e4712a5e12
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-10-09 18:15:51 +0200

    successfully build the userModel and also implemented a create API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const userModel = require('./models/users')
 
 const app = express()
 app.use(express.json())
@@ -9,7 +10,9 @@ app.use(cors())
 mongoose.connect("mongodb://localhost:27017/lxd");
 
 app.post('/users', (req, res) => {
-    
+    userModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
 })
 
 app.listen(3001, () => {
--- models/users.js
+++ models/users.js
@@ -7,3 +7,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
+
+const userModel = mongoose.model("users", userSchema)
+module.exports = userModel
