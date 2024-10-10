e0fab01a05bf0de5d65b316374020efb2be8f1f6
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-10-10 11:05:44 +0200

    added the log in backend logic

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ app.use(cors())
 
 mongoose.connect("mongodb://localhost:27017/lxd");
 
+app.post('/login',(req, res) => {
+    const {email, password} = req.body;
+    userModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            if(user.password === password){
+                res.json("log in successful")
+            } else {
+                res.json("the password is incorrect")
+            }
+        } else {
+            res.json("Email is not registered try to register")
+        }
+    })
+})
+
 app.post('/users', (req, res) => {
     userModel.create(req.body)
     .then(users => res.json(users))
